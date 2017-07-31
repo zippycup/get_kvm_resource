a1216cde70e231d0cc733d8a7ebc02154dec5017
zippycup <zippycup@hyee-test-01.lga1.admarketplace.net>
2017-07-31 09:28:25 -0400

    init put

---
+++ README.md
@@ -0,0 +1,23 @@
+# puppet-pre-commit-script
+
+## Description
+
+Bash script to check resource such as cpu and memory of a kvm host
+
+
+## Installation
+
+git clone or download file(s) to your host
+note the path to get_kvm_resource.sh
+
+### Requirements
+
+installed the following software:
+for centos rpm
+* libvirt-client
+
+
+## Run utility
+
+* cd [to git or svn repo directory]
+* sh [path_to_file]/get_kvm_resource.sh
---
+++ get_kvm_resource.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+all_host=`virsh list --all | egrep 'running|off' | awk '{print $2}'`
+running_host=`virsh list --all | grep 'running' | awk '{print $2}'`
+
+running_cpu=0
+running_memory=0
+all_cpu=0
+all_memory=0
+
+echo 'Running CPU'
+for i in $running_host
+do
+  current_cpu=`virsh dominfo $i | grep 'CPU(s)' | awk '{print $2}'`
+  echo "$i : ${current_cpu}"
+  ((running_cpu=$running_cpu + $current_cpu))
+done
+echo
+
+echo 'Allocated CPU'
+for i in $all_host
+do
+  current_cpu=`virsh dominfo $i | grep 'CPU(s)' | awk '{print $2}'`
+  echo "$i : ${current_cpu}"
+  ((all_cpu=$all_cpu + $current_cpu))
+done
+echo
+
+echo 'Running memory'
+for i in $running_host
+do
+  current_memory=`virsh dominfo $i | grep 'Max memory' | awk '{print $3}'`
+  echo "$i : ${current_memory}"
+  ((running_memory=$running_memory + $current_memory))
+done
+echo
+
+
+echo 'Allocated memory'
+for i in $all_host
+do
+  current_memory=`virsh dominfo $i | grep 'Max memory' | awk '{print $3}'`
+  echo "$i : ${current_memory}"
+  ((all_memory=$all_memory + $current_memory))
+done
+echo
+
+echo "Host summary"
+virsh nodeinfo
+
+host_cpu=`virsh nodeinfo | grep 'CPU(s)' | awk '{print $2}'`
+host_memory=`virsh nodeinfo | grep 'Memory size' | awk '{print $3}'`
+
+((reserve_memory=($host_memory - $running_memory) / 1000000 ))
+
+echo "all_cpu        : ${all_cpu}"
+echo "running_cpu    : ${running_cpu}"
+echo "host_cpu       : ${host_cpu}"
+echo
+echo "all_memory     : ${all_memory} KiB"
+echo "running_memory : ${running_memory} KiB"
+echo "host_memory    : ${host_memory} KiB"
+echo
+echo "reserve memory : ${reserve_memory} Gb"
